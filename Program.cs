959330e2a92fa689b267ca96edd96d4aa032e511
Prakash Beegala <prakash.beegala@microsoft.com>
2026-02-26 00:24:17 +0000

    Fix HTTPS redirection for Container Apps deployment

--- Program.cs
+++ Program.cs
@@ -33,8 +33,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    // Only use HTTPS redirection in development
+    app.UseHttpsRedirection();
+}
 app.UseStaticFiles();
 
 app.UseRouting();
